504f4308c7d16260d2451b26dc66b5436a5be293
Busiswa <onakomini@gmail.com>
2020-11-30 22:39:44 +0200

    tests

--- test/waiter.test.js
+++ test/waiter.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const pg = require("pg");
 const Pool = pg.Pool;
-const waiters = require("../waiters")
+const waiter = require("../waiters")
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://codex-coder:pg123@localhost:5432/waiters_availability'
 
@@ -12,7 +12,9 @@ const pool = new Pool({
 
 
 
-const waiter = waiters(pool)
+
+const waiterAvailability = waiter(pool)
+
 describe('waiters Availability', function () {
 
     beforeEach(async function () {
@@ -24,96 +26,47 @@ describe('waiters Availability', function () {
 
     });
 
-    it('should be able to insert names in the  database', async () => {
-        await waiter.geWaiter('Phelisa');
-       // await regNum.addRegNumber('CA 123-123');
-        //await regNum.addRegNumber('CJ 124-124');
+    it('should be able to insert names in the  database and show the name in a list', async () => {
+        await waiterAvailability.getWaiter('Phelisa');
 
 
-        const regsList = await regNum.showReg()
+
+
+        const nameList = await waiterAvailability.showWaiters()
 
         assert.deepEqual([
             {
-                reg_number: 'CL 123 321',
-                town_id: 3
-            },
-            {
-                reg_number: 'CA 123-123',
-                town_id: 2
-            },
-            {
-                reg_number: 'CJ 124-124',
-                town_id: 1
+                waiter_name: 'Phelisa'
             }
-        ]
-            , regsList);
+        ], nameList
+        )
 
     });
-    // it("should not be able to add registration numbers that already exists  in the database", async () => {
-    //     await regNum.addRegNumber('CL 123 321');
-    //     await regNum.addRegNumber('CA 123-123');
-    //     await regNum.addRegNumber('CJ 124-124');
-    //     await regNum.addRegNumber('CA 123-123');
-    //     await regNum.addRegNumber('CJ 124-124');
+    it("should not be able to show a list of weekdays", async () => {
+        const alldays = await waiterAvailability.allDays()
 
-    //     const exists = await regNum.showReg()
-    //     assert.deepEqual([
-    //         {
-    //             reg_number: 'CL 123 321',
-    //             town_id: 3
-    //         },
-    //         {
-    //             reg_number: 'CA 123-123',
-    //             town_id: 2
-    //         },
-    //         {
-    //             reg_number: 'CJ 124-124',
-    //             town_id: 1
-    //         }
-    //     ]
-    //         , exists);
-    // })
+        assert.deepEqual([{
+        day_name: 'Sunday'},
+        {day_name: 'Monday'},
+        { day_name: 'Tuesday' },
+        { day_name: 'wednesday' },
+        { day_name: 'Thursday' },
+        { day_name: 'Friday' },
+        { day_name: 'Satarday' },
+        ], alldays)
+    })
 
-    // it("should be able to filter registrations from  cape town ", async () => {
-    //     await regNum.addRegNumber('CL 123 321');
-    //     await regNum.addRegNumber('CA 123');
-    //     await regNum.addRegNumber('CJ 124-124');
-    //     await regNum.addRegNumber('CA 123-123');
-    //     await regNum.addRegNumber('CA 124-124');
+     it("should be able to get the id of a waiter and the day id ", async () => {
+    const ids = await waiterAvailability.getIds();
+    assert.deepEqual([{waiter_name_id : '1'},
+    {day_name_id : '1',ids}
+])
 
-    //     const filterCapeTown = await regNum.filter('2');
-        
-    //     assert.deepEqual([
-    //         {
-    //           reg_number: 'CA 123'
-    //         },
-    //         {
-    //           reg_number: 'CA 123-123'
-    //         },
-    //         {
-    //           reg_number: 'CA 124-124'
-    //         }
-    //       ]
-    //       ,filterCapeTown );
+     });
+     it("should be able to  ", async () => {
+    
 
-    // });
-    // it("should be able to filter registrations from  Paarl ", async () => {
-    //     await regNum.addRegNumber('CL 123 321');
-    //     await regNum.addRegNumber('CA 123');
-    //     await regNum.addRegNumber('CJ 124-124');
-    //     await regNum.addRegNumber('CA 123-123');
-    //     await regNum.addRegNumber('CA 124-124');
-
-    //     const filterPaarl = await regNum.filter('1');
-        
-    //     assert.deepEqual([
-    //         {
-    //           reg_number: 'CJ 124-124'
-    //         }
-    //       ]
-    //       , filterPaarl);
-
-    // });
+     });
     // it("should be able to filter registrations from  Stellenbosch", async () => {
     //     await regNum.addRegNumber('CL 123 321');
     //     await regNum.addRegNumber('CA 123');
@@ -122,7 +75,7 @@ describe('waiters Availability', function () {
     //     await regNum.addRegNumber('CA 124-124');
 
     //     const filterStellenbosch = await regNum.filter('3');
-        
+
     //     assert.deepEqual([
     //         {
     //           reg_number: 'CL 123 321'
@@ -166,7 +119,7 @@ describe('waiters Availability', function () {
     //     await regNum.addRegNumber('CA 123-123');
     //     await regNum.addRegNumber('CA 124-124');
 
-        
+
     //     await regNum.resetData()
     // const reset=await regNum.showReg()
     //     assert.deepEqual([],reset);
